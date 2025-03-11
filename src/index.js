4291a6879187ad46a88ecb4809b58d44e1e47baa
rithish72 <rithishkurapati72@gmail.com>
2025-03-11 14:28:30 +0530

    Add Express server setup with CORS and cookie-parser integration; implement API error and response handling utilities

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import dotenv from 'dotenv';
 import { connectDB } from './db/index.js';
 
+
 dotenv.config({
     path: '/.env' 
 });
 
-connectDB(); 
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+}
+)
+.catch((err) => {
+    console.log("DB connection failed", err);
+}
+);
